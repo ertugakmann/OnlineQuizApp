d646d3e9b046c214d8fe6eae57653307e8d65784
ertugakmann <134059022+ertugakmann@users.noreply.github.com>
2024-01-13 17:02:49 +0300

    I rendered quiz cards in box.

--- OnlineQuizApp/src/components/Feed.jsx
+++ OnlineQuizApp/src/components/Feed.jsx
@@ -1,11 +1,25 @@
-import { Box } from "@mui/material";
 import React from "react";
+import Quiz from "./Quiz";
+import { Box, Typography } from "@mui/material";
+import { pink } from "@mui/material/colors";
 
 function Feed() {
   return (
-    <Box bgcolor={"pink"} flex={1} p={2}>
-      Feed
-    </Box>
+    <>
+      <Box flex={4} p={2}>
+        <Typography variant="h3" flexGrow={1} textAlign={"center"}>
+          Popular Quizs
+        </Typography>
+        <Box mt={1} sx={{ display: "flex", flexWrap: "wrap", gap: "30px" }}>
+          <Quiz />
+          <Quiz />
+          <Quiz />
+          <Quiz />
+          <Quiz />
+          <Quiz />
+        </Box>
+      </Box>
+    </>
   );
 }
 
