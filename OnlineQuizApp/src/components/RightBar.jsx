0f468a6320267b1b99dc55e24a84b37027f1005c
ertugakmann <134059022+ertugakmann@users.noreply.github.com>
2024-01-14 20:47:29 +0300

    I  added a Right Bar to my page.

--- OnlineQuizApp/src/components/RightBar.jsx
+++ OnlineQuizApp/src/components/RightBar.jsx
@@ -1,15 +1,42 @@
-import { Box } from "@mui/material";
+import {
+  Avatar,
+  Box,
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  Typography,
+} from "@mui/material";
 import React from "react";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import FavoriteQuiz from "./FavoriteQuiz";
 
 function RightBar() {
   return (
-    <Box
-      bgcolor={"skyblue"}
-      flex={1}
-      p={2}
-      sx={{ display: { xs: "none", sm: "block" } }}
-    >
-      Rightbar
+    <Box flex={1} paddingTop={2} sx={{ display: { xs: "none", sm: "block" } }}>
+      <Box>
+        <Typography display="flex" justifyContent="center" variant="h4">
+          Favorite Quizs
+        </Typography>
+        <Box sx={{ display: "flex", justifyContent: "center" }}>
+          {/* This box true there?*/}
+
+          <Avatar
+            sx={{
+              bgcolor: "red",
+            }}
+          >
+            <FavoriteIcon />
+          </Avatar>
+        </Box>
+        <Box marginTop={3} marginRight={1}>
+          <FavoriteQuiz />
+          <FavoriteQuiz />
+          <FavoriteQuiz />
+          <FavoriteQuiz />
+          <FavoriteQuiz />
+        </Box>
+      </Box>
     </Box>
   );
 }
