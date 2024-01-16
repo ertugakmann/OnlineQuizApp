f210881713a6c0e280e144611befa2c27b31bea8
ertugakmann <134059022+ertugakmann@users.noreply.github.com>
2024-01-16 10:03:27 +0300

    I moved StyledList to another component and created a StyledSideBar.

--- OnlineQuizApp/src/components/Sidebar.jsx
+++ OnlineQuizApp/src/components/Sidebar.jsx
@@ -1,113 +1,31 @@
-import React from "react";
-import {
-  Box,
-  List,
-  ListItem,
-  ListItemButton,
-  ListItemIcon,
-  ListItemText,
-  Typography,
-} from "@mui/material";
-import ClassIcon from "@mui/icons-material/Class";
+import { Box, Typography } from "@mui/material";
+import * as React from "react";
+import MyQuiz from "./MyQuiz";
 import styled from "@emotion/styled";
 
 function Sidebar() {
-  const StyledList = styled(List)({
-    marginBottom: "15px",
-    width: "250px",
+  const StyledSideBar = styled(Box)({
+    backgroundColor: "white",
+    paddingTop: "15px",
+    paddingLeft: "20px",
+    position: "relative",
+    left: "-8px",
   });
-
+  {
+    /*Is this position true?*/
+  }
   return (
-    <Box
-      flex={1}
-      sx={{ display: { xs: "none", sm: "block" }, paddingTop: "15px" }}
-    >
+    <StyledSideBar>
+      <Typography variant="h3" sx={{ color: "#0F1035" }}>
+        MY QUIZS
+      </Typography>
       <Box>
-        <Typography variant="h3" sx={{ color: "#0F1035" }}>
-          MY QUIZS
-        </Typography>
-        <StyledList>
-          <ListItem disablePadding>
-            <ListItemButton component="a" href="#">
-              <ListItemIcon>
-                <ClassIcon />
-              </ListItemIcon>
-              <ListItemText primary="Quiz 1" />
-            </ListItemButton>
-          </ListItem>
-        </StyledList>
-        <StyledList>
-          <ListItem disablePadding>
-            <ListItemButton component="a" href="#">
-              <ListItemIcon>
-                <ClassIcon />
-              </ListItemIcon>
-              <ListItemText primary="Quiz 2" />
-            </ListItemButton>
-          </ListItem>
-        </StyledList>
-        <StyledList>
-          <ListItem disablePadding>
-            <ListItemButton component="a" href="#">
-              <ListItemIcon>
-                <ClassIcon />
-              </ListItemIcon>
-              <ListItemText primary="Quiz 3" />
-            </ListItemButton>
-          </ListItem>
-        </StyledList>
-        <StyledList>
-          <ListItem disablePadding>
-            <ListItemButton component="a" href="#">
-              <ListItemIcon>
-                <ClassIcon />
-              </ListItemIcon>
-              <ListItemText primary="Quiz 4" />
-            </ListItemButton>
-          </ListItem>
-        </StyledList>
-        <StyledList>
-          <ListItem disablePadding>
-            <ListItemButton component="a" href="#">
-              <ListItemIcon>
-                <ClassIcon />
-              </ListItemIcon>
-              <ListItemText primary="Quiz 5" />
-            </ListItemButton>
-          </ListItem>
-        </StyledList>
-        <StyledList>
-          <ListItem disablePadding>
-            <ListItemButton component="a" href="#">
-              <ListItemIcon>
-                <ClassIcon />
-              </ListItemIcon>
-              <ListItemText primary="Quiz 6" />
-            </ListItemButton>
-          </ListItem>
-        </StyledList>
-        <StyledList>
-          <ListItem disablePadding>
-            <ListItemButton component="a" href="#">
-              <ListItemIcon>
-                <ClassIcon />
-              </ListItemIcon>
-              <ListItemText primary="Quiz 7" />
-            </ListItemButton>
-          </ListItem>
-        </StyledList>
-        <StyledList>
-          <ListItem disablePadding>
-            <ListItemButton component="a" href="#">
-              <ListItemIcon>
-                <ClassIcon />
-              </ListItemIcon>
-              <ListItemText primary="Quiz 8" />
-            </ListItemButton>
-          </ListItem>
-        </StyledList>
+        <MyQuiz />
+        <MyQuiz />
+        <MyQuiz />
+        <MyQuiz />
       </Box>
-    </Box>
+    </StyledSideBar>
   );
 }
 
