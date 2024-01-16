7322ac3975eafddbbb6d55899a9441cc3a196a01
ertugakmann <134059022+ertugakmann@users.noreply.github.com>
2024-01-16 09:52:31 +0300

    I changed some style codes and removed needless imports.

--- OnlineQuizApp/src/components/Feed.jsx
+++ OnlineQuizApp/src/components/Feed.jsx
@@ -1,13 +1,12 @@
 import React from "react";
 import Quiz from "./Quiz";
 import { Box, Typography } from "@mui/material";
-import { pink } from "@mui/material/colors";
 
 function Feed() {
   return (
     <>
-      <Box flex={5} p={2}>
-        <Typography variant="h3" flexGrow={1} textAlign={"center"}>
+      <Box bgcolor={"whitesmoke"} flex={5} p={2}>
+        <Typography variant="h3" textAlign={"center"}>
           Popular Quizs
         </Typography>
         <Box mt={1} sx={{ display: "flex", flexWrap: "wrap", gap: "30px" }}>
