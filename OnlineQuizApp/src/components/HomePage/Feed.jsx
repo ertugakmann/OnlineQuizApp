46d85f5165bf18f7cfec0511ad3038d2f1779deb
ertugakmann <134059022+ertugakmann@users.noreply.github.com>
2024-02-14 18:22:31 +0300

    Responsive bugs fixed.

--- OnlineQuizApp/src/components/HomePage/Feed.jsx
+++ OnlineQuizApp/src/components/HomePage/Feed.jsx
@@ -5,11 +5,24 @@ import { Box, Typography } from "@mui/material";
 function Feed() {
   return (
     <>
-      <Box bgcolor={"whitesmoke"} flex={5} p={2} pl={{ xs: "3px", sm: 8 }}>
+      <Box
+        sx={{
+          backgroundColor: "whitesmoke",
+          justifyContent: "center",
+        }}
+        p={2}
+      >
         <Typography variant="h3" textAlign={"center"}>
           Popular Quizs
         </Typography>
-        <Box mt={1} sx={{ display: "flex", flexWrap: "wrap", gap: "30px" }}>
+        <Box
+          mt={1}
+          sx={{
+            display: "flex",
+            flexWrap: "wrap",
+            gap: "30px",
+          }}
+        >
           <Quiz />
           <Quiz />
           <Quiz />
